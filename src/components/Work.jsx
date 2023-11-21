031dce843d489b64feefad285694a1214ff06913
obed-tc <castroobed53@gmail.com>
2023-11-21 19:07:14 -0400

    Se modifico componente Work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,34 +1,67 @@
 import Card from "./common/card";
-
+import { FaComputer } from "react-icons/fa6";
+import { FaMobileAlt } from "react-icons/fa";
+import { TbWorld } from "react-icons/tb";
+import { FaSourcetree } from "react-icons/fa";
+import { MdOutlineWbCloudy } from "react-icons/md";
+import { FaRegFileCode } from "react-icons/fa";
+import { CiServer } from "react-icons/ci";
 function Work() {
   return (
-    <div id="work" className="relative pt-[100px]">
-
+    <div id="work" className="relative pt-[100px] bg-[#161f28] pb-[100px]">
       <div className="relative z-20 px-5 ">
         <div className="">
-          <div>
-            <p className="text-gray-600 ">*/*</p>
+          <div className="text-center">
 
-            <h1 className="text-primary-600 text-center sm:text-left font-bold text-[50px]">
+            <h1 className="text-primary-600 text-center sm:text-center font-bold text-[50px]">
               Lo que hacemos
             </h1>
-            <p className="text-gray-600 ">*/*</p>
-            <p className="sm:w-[50vw]">Desarrollamos sistemas a la medida siguiendo estrictos estándares de calidad. Nuestras aplicaciones son escalables, robustas y fáciles de usar, y trabajamos solo con las principales tecnologías acorde a las necesidades de cada proyecto</p>
+            <div className="flex justify-center">
+              <div className=" w-[200px] h-[3px] mb-[20px] bg-primary-500"></div>
+
+            </div>
+            <p className="sm:w-full text-center">Desarrollamos sistemas a la medida siguiendo estrictos estándares de calidad. Nuestras aplicaciones son escalables, robustas y fáciles de usar, y trabajamos solo con las principales tecnologías acorde a las necesidades de cada proyecto</p>
           </div>
 
           {/* contenido de las cards */}
-          <div className="flex flex-wrap space-y-5  sm:space-x-5 pt-[50px]">
+          <div className="flex flex-wrap justify-center  pt-[50px]">
+
             <Card
+              icon={< FaMobileAlt/>}
               title="Desarrollo de apps moviles"
               description="Sistemas para todo tipo de dispositivos y plataformas móviles. Maneja el mundo desde tus manos."
-              imageUrl="https://png.pngtree.com/thumb_back/fw800/background/20230716/pngtree-portfolio-displays-and-mobile-apps-with-3d-shapes-chat-messages-and-image_3879137.jpg"
-              tags={["#Flutter", "#React Native", "#Ionic"]}
             />
                         <Card
+              icon={< FaComputer/>}
+
+              title="Desarrollo de apps moviles"
+              description="Sistemas para todo tipo de dispositivos y plataformas móviles. Maneja el mundo desde tus manos."
+            />
+                        <Card
+                        icon={<TbWorld></TbWorld>}
+              title="Desarrollo de apps moviles"
+              description="Sistemas para todo tipo de dispositivos y plataformas móviles. M aneja el mundo desde tus manos."
+            />
+                        <Card
+                        icon={<MdOutlineWbCloudy></MdOutlineWbCloudy>}
+              title="Desarrollo de apps moviles"
+              description="Sistemas para todo tipo de dispositivos y plataformas móviles. Maneja el mundo desde tus manos."
+            />
+                        <Card
+                        icon={<FaSourcetree></FaSourcetree>}
+              title="Desarrollo de apps moviles"
+              description="Sistemas para todo tipo de dispositivos y plataformas móviles. Maneja el mundo desde tus manos."
+            />
+
+                        <Card
+                        icon={<FaRegFileCode />}
               title="Desarrollo web"
               description="Sistemas para todo tipo de dispositivos y plataformas móviles. Maneja el mundo desde tus manos."
-              imageUrl="https://img.freepik.com/foto-gratis/experiencia-programacion-persona-que-trabaja-codigos-computadora_23-2150010125.jpg"
-              tags={["#Angular"]}
+            />
+                                    <Card
+                        icon={<CiServer />}
+              title="Desarrollo web"
+              description="Sistemas para todo tipo de dispositivos y plataformas móviles. Maneja el mundo desde tus manos."
             />
             
 
--- src/components/common/card.jsx
+++ src/components/common/card.jsx
@@ -1,30 +1,23 @@
-
-const Card = ({ title, description, imageUrl, tags }) => {
+const Card = ({ title, description, icon }) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white bg-opacity-10 ">
-      <img
-        className="w-full"
-        src={imageUrl}
-        alt={title}
-      />
+    <div className="mt-[20px] mx-[10px] max-w-sm h-[300px] rounded-lg overflow-hidden shadow-lg bg-white bg-opacity-10 ">
       <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">
-          {title}
+        <div className="text-[60px] my-[20px] text-primary-500">
+          {icon}
         </div>
-        <p className="text-gray-100 text-base">
-          {description}
-        </p>
+        <div className="">
+          <div className="font-bold text-xl mb-2 border border-b-2 border-transparent text-primary-500">
+            {title}
+          </div>
+          <div className=" w-full h-[3px] mb-[20px] bg-primary-500"></div>
+          <p className="text-gray-100 text-sm">
+            {description}
+          </p>
+        </div>
+
       </div>
-      <div className="px-6 pt-4 pb-2">
-        {tags.map((tag, index) => (
-          <span
-            key={index}
-            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-          >
-            {tag}
-          </span>
-        ))}
-      </div>
+
+
     </div>
   );
 };
